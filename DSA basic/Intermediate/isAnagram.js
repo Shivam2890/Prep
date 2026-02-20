e039591cce6c5e2ce0abf461f9f7d22c802371d2
Shivam2890 <Shivamkori2890@gmail.com>
2026-02-20 17:57:26 +0530

    set map solution of anagram

--- DSA basic/Intermediate/isAnagram.js
+++ DSA basic/Intermediate/isAnagram.js
@@ -1,5 +1,53 @@
 function isAnagram(s1, s2) {
-  // your code
+  if (s1.length != s2.length) return false
+  let arr1 = [...s1]
+  let arr2 = [...s2]
+
+  for (let item of arr1) {
+
+    if (!arr2.includes(item)) {
+      return false
+    } else {
+      let index = arr2.indexOf(item)
+      arr2.splice(index, 1)
+    }
+
+  }
+  return true
+
 }
 
-console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("aaccq", "caacq"));
+
+function isAnagram(s1, s2) {
+  if (s1.length !== s2.length) return false;
+
+  let obj = {}
+
+  for (let ch of s1) {
+    obj[ch] = (obj[ch] ?? 0) + 1
+  }
+  for(let ch of s2){
+    if(!obj[ch]) return false
+    obj[ch]--  
+  }
+  return true
+}
+
+console.log(isAnagram("listten", "silentt"));
+
+function isAnagram(s1, s2) {
+  let map = new Map()
+  for (let ch of s1) {
+    let count = (map.get(ch) ?? 0) + 1
+    map.set(ch, count)
+  }
+  for (let ch of s2) {
+    if (!map.has(ch) || map.get(ch) === 0) return false
+    map.set(ch, map.get(ch) - 1)
+  }
+  console.log(map)
+  return true
+}
+
+console.log(isAnagram("listten", "silentt"));
